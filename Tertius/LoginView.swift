0c2687a69dab3c30a41488b2ed3b9f71b4546219
mac <heyucong961028@berkeley.edu>
2015-09-20 00:01:00 -0400

    Sign uped

--- Tertius/LoginView.swift
+++ Tertius/LoginView.swift
@@ -11,13 +11,39 @@ import UIKit
 import Parse
 
 class LoginView: UIViewController {
-
+    
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
     }
     
-    func login
+    func login(username: String, password: String) {
+        
+        var error: NSErrorPointer?
+        
+        if let user = PFUser.logInWithUsername(username, password: password, error: error!) {
+            print(user)
+        } else {
+            print(error)
+        }
+    }
+    
+    func signup(username: String, password: String, email: String) {
+        var user = PFUser()
+        user.username = username
+        user.password = password
+        user.email = email
+        user.signUpInBackgroundWithBlock { (succeeded: Bool, error: NSError?) -> Void in
+            if succeeded {
+                print("Sign up successfully with \(user.username)")
+            } else {
+                print("An error occured with: \(error)")
+            }
+        }
+    }
+    
+    
+    
+    
     
 }
