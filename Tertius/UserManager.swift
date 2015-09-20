dbc26f87bd0f4cc7f46a63d8e42b9430a29b0405
Zizheng Tai <zizheng.tai@gmail.com>
2015-09-20 01:54:17 -0400

    improved models

---
+++ Tertius/UserManager.swift
@@ -0,0 +1,32 @@
+//
+//  UserManager.swift
+//  Tertius
+//
+//  Created by Zizheng Tai on 9/20/15.
+//  Copyright © 2015 Ryan Li. All rights reserved.
+//
+
+class UserManager {
+    
+    static let sharedInstance = UserManager()
+    
+    func getMessagesForCurrentUser(block: ([Message]?, NSError?) -> Void) {
+        let query = Treazure.query()!
+        query.whereKey("user", equalTo: User.currentUser()!)
+        
+        query.findObjectsInBackgroundWithBlock { treazures, error in
+            if let treazures = treazures {
+                let query = Message.query()!
+                query.whereKey("treazure", containedIn: treazures)
+                
+                query.findObjectsInBackgroundWithBlock { messages, error in
+                    if let messages = messages as? [Message] {
+                        block(messages, nil)
+                    }
+                }
+            } else {
+                block(nil, error)
+            }
+        }
+    }
+}
