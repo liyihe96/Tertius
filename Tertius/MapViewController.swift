19e448eb58569bee22c248b72e5016202629e2fb
liyihe96 <liyihe96@gmail.com>
2015-09-20 05:47:58 -0400

    improve UI

--- Tertius/MapViewController.swift
+++ Tertius/MapViewController.swift
@@ -47,6 +47,17 @@ class MapViewController: UIViewController {
                 marker.map = self.mapView
             }
         }
+
+        UserManager.sharedInstance.getMessagesFoundByCurrentUser { messages, error in
+            if let error = error {
+                NSLog("Error %@", error.localizedDescription)
+            }
+            NSLog("Message: %@", messages!)
+            for message in messages! {
+                let marker = PlaceMarker(message: message, placeType: .PicketUpFrom)
+                marker.map = self.mapView
+            }
+        }
     }
 }
 
@@ -60,10 +71,15 @@ extension MapViewController: CLLocationManagerDelegate {
     }
 
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        struct ConstVar {
+            static var firstShown = true
+        }
         if let location = locations.first as CLLocation? {
 
-            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 20, bearing: 0, viewingAngle: 0)
-
+            if ConstVar.firstShown {
+                mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 20, bearing: 0, viewingAngle: 0)
+                ConstVar.firstShown = false
+            }
             NSLog("Current Location: %@", location)
             User.currentUser()!.currentLocation = PFGeoPoint(location: location)
             User.currentUser()!.saveInBackground()
@@ -74,5 +90,29 @@ extension MapViewController: CLLocationManagerDelegate {
 extension MapViewController: GMSMapViewDelegate {
     func mapView(mapView: GMSMapView!, idleAtCameraPosition position: GMSCameraPosition!) {
 
+    }
+    func mapView(mapView: GMSMapView!, didTapMarker marker: GMSMarker!) -> Bool {
+//        mapCenterPinImage.fadeOut(0.25)
+        return false
+    }
+    func mapView(mapView: GMSMapView!, markerInfoContents marker: GMSMarker!) -> UIView! {
+        // 1
+        let placeMarker = marker as! PlaceMarker
+
+        // 2
+        if let infoView = MarkerInfoView.instanceFromNib() {
+            // 3
+            infoView.nameLabel.text = placeMarker.address
+            infoView.placePhoto.image = UIImage(named: "Treasure")
+            infoView.placePhoto.contentMode = .ScaleAspectFit
+            return infoView
+        } else {
+            return nil
+        }
+    }
+
+    func didTapMyLocationButtonForMapView(mapView: GMSMapView!) -> Bool {
+        mapView.selectedMarker = nil
+        return false
     }
 }
