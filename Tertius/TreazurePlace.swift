5285e3f8157767c00f9baf372396a44766c461c7
liyihe96 <liyihe96@gmail.com>
2015-09-20 02:32:23 -0400

    Add TreazurePlace class

---
+++ Tertius/TreazurePlace.swift
@@ -0,0 +1,13 @@
+//
+//  TreazurePlace.swift
+//  Tertius
+//
+//  Created by Ryan Li on 9/20/15.
+//  Copyright © 2015 Ryan Li. All rights reserved.
+//
+
+import Cocoa
+
+class TreazurePlace: NSObject {
+
+}
